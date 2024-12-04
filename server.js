07267fcceb616081edc8bca38153032e601f7d45
Abdulrahman-Abdulhak <bdalrhmanbdalhq@gmail.com>
2024-12-04 22:30:33 +0300

    routers and controllers init setup

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from "express";
+import { tasksRouter } from "./routes/index.js";
 
 const app = express();
 const port = 3000;
 
+//* middlewares
+app.use(express.json());
+
+//* routes
+
 app.get("/hello", (req, res) => {
-  res.send("Task Manage App");
+  res.send("Task Manager App");
 });
+
+app.use("/api/v1/tasks", tasksRouter);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
